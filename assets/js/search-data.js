47bbfa06672ceb37da59b2d0293367047e373a4f
coco2-vicc <nickford@uw.edu>
2026-09-12 23:39:27 +0000

    Deploying to gh-pages from @ coco2-vicc/coco2-vicc.github.io@f7d3ea7fe4def089cdaada2a888925a74e321f86 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -35,7 +35,7 @@ ninja.data = [{
           description: "",
           section: "Navigation",
           handler: () => {
-            window.location.href = "/data";
+            window.location.href = "/capacitysharing";
           },
         },{id: "nav-news",
           title: "news",
