de7468ea893ec3c269863eb71bfffbcc94a4aff8
coco2-vicc <nickford@uw.edu>
2026-09-12 23:22:33 +0000

    Deploying to gh-pages from @ coco2-vicc/coco2-vicc.github.io@a117f6f32da1100ee5eff1add45b310e241178ac 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -18,7 +18,7 @@ ninja.data = [{
           },
         },{id: "nav-team",
           title: "Team",
-          description: "COCO2 team members",
+          description: "",
           section: "Navigation",
           handler: () => {
             window.location.href = "/team/";
